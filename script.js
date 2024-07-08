e4e7a0dde5f4c206b2ee7fc20c56c1ee1358b57d
Elijah Odjegba <elijahodjegba@gmail.com>
2024-07-08 16:57:39 +0100

    update links

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   function updateTime() {
     const now = new Date();
-    const utcTime = now.toUTCString().split(" ")[4];
-    const day = now.toLocaleString("en-US", { weekday: "long" });
 
+    // Calculate Nigerian time (GMT+1)
+    now.setHours(now.getHours() + 1);
+
+    // UTC time
+    const utcTime = now.toUTCString().split(" ")[4];
+    // Day of the week
+    const day = now.toLocaleString("en-US", { weekday: "long" });
+    // Update HTML elements
     document.getElementById("utcTime").textContent = utcTime;
     document.getElementById("currentDay").textContent = day;
   }
